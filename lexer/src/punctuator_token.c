fb2dbd7823c3a2302c4e2ce6a0fbe3a0f65ab9e0
Dario Sneidermanis <dariosn@gmail.com>
2011-09-23 16:08:57 -0300

    movings things around, and style refactor

---
+++ lexer/src/punctuator_token.c
@@ -0,0 +1,82 @@
+#include <string.h>
+#include <stdlib.h>
+
+
+static const char punctuators[][5] = {
+	"",
+	"!",
+	"!=",
+	"#",
+	"##",
+	"%",
+	"%:",
+	"%:%:",
+	"%=",
+	"%>",
+	"&",
+	"&&",
+	"&=",
+	"(",
+	")",
+	"*",
+	"*=",
+	"+",
+	"++",
+	"+=",
+	",",
+	"-",
+	"--",
+	"-=",
+	"->",
+	".",
+	"...",
+	"/",
+	"/=",
+	":",
+	":>",
+	";",
+	"<",
+	"<%",
+	"<:",
+	"<<",
+	"<<=",
+	"<=",
+	"=",
+	"==",
+	">",
+	">=",
+	">>",
+	">>=",
+	"?",
+	"[",
+	"]",
+	"^",
+	"^=",
+	"{",
+	"|",
+	"|=",
+	"||",
+	"}",
+	"~"
+};
+
+
+#define NUM_PUNCTUATORS     (sizeof(punctuators) / sizeof(punctuators[0]))
+#define MAX_LEN_PUNCTUATORS sizeof(punctuators[0])
+
+
+typedef int (*cmp_f)( const void*, const void* );
+
+
+enum punctuator_token is_punctuator(const char* token) {
+
+	const char (*pos)[ MAX_LEN_PUNCTUATORS ] =
+		bsearch(token, punctuators, NUM_PUNCTUATORS, MAX_LEN_PUNCTUATORS, (cmp_f)strcmp);
+
+	if (!pos) {
+		pos = punctuators;
+    }
+
+	return pos - punctuators;
+}
+
